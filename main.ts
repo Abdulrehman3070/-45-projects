8f1ca7045b29c864926a4f32d21550a3ed45acf6
[Abdul Rehman] <[rehmankhana435@gmail.com]>
2024-02-23 02:44:21 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,9 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+//Let Store a person’s name in a variable
+let personName : string = "Abdul Rehman";
+//Let create the message
+let message: string = `Hello ${personName}, would you like to learn some Python today?`
+//let print message in console
+console.log(message)
+
